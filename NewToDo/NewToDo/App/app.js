d49dfdbc3da96cc0f0db56906e705a5fc70960d9
Josh Kilgore <kilgore.josh@gmail.com>
2017-05-30 19:04:01 -0500

    add controllers and enable scripts and migrations

---
+++ NewToDo/NewToDo/App/app.js
@@ -0,0 +1,28 @@
+﻿var lastGroup = angular.module("Authentication", ["ngRoute"]);
+
+lastGroup.config([
+    "$routeProvider", function ($routeProvider) {
+        $routeProvider
+            .when("/",
+            {
+                templateUrl: "lastGroup/login.html",
+                controller: "loginController"
+            })
+            .when("/home",
+            {
+                templateUrl: "lastGroup/home.html",
+                controller: "homeController"
+            });
+
+    }
+])
+
+lastGroup.run(["$http", function ($http) {
+
+    var token = sessionStorage.getItem('token');
+
+    if (token)
+        $http.defaults.headers.common['Authorization'] = `bearer ${token}`;
+
+}
+]);
---
+++ NewToDo/NewToDo/App/homeController.js
@@ -0,0 +1,8 @@
+﻿lastGroup.controller("homeController", ["$scope", "$http", function ($scope, $http) {
+    $scope.values = [];
+
+    $http.get("/api/values")
+        .then(function (result) {
+            $scope.values = result.data;
+        });
+}])
